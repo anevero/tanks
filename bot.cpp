3b165b2d6cbc0369d928aea8413c337aa40cd2a1
Lessyless <40501121+Lessyless@users.noreply.github.com>
2019-04-02 12:18:03 +0300

    Merge pull request #32
    Добавлены двигающиеся роботы, улучшена логика из стрельбы. Добавлен предпросчет стен на карте.

--- bot.cpp
+++ bot.cpp
@@ -1,52 +1,108 @@
 #include "bot.h"
 
 Bot::Bot(std::shared_ptr<Map>& map, int init_cell_x, int init_cell_y, int speed,
-         int rate_of_fire, Direction direction)
-    : Tank(map, init_cell_x, init_cell_y, speed, rate_of_fire, direction) {
+         int rate_of_fire, Direction direction, int moving_length, int amount_of_turns)
+    : Tank(map, init_cell_x, init_cell_y, speed, rate_of_fire, direction),
+      moving_length_(moving_length),
+      amount_of_turns_(amount_of_turns) {
   image_.load(":/textures/bot.png");
   scaled_image_ = image_;
 };
 
-bool Bot::IsTurnNeeded() const { return time_to_finish_rotation_ > 0; }
+bool Bot::IsTurnNeeded() const {
+  return time_to_finish_rotation_ > 0;
+}
 
-bool Bot::IsRotationStartNeeded() const {
-  return time_to_finish_rotation_ <= 0;
+bool Bot::IsRotationStartNeeded(std::shared_ptr<Tank>) {
+  if (time_to_finish_rotation_ <= 0 && time_to_finish_movement_ <= 0) {
+    if (number_of_turns_ > 0) {
+      number_of_turns_--;
+      return number_of_turns_ > 0 ? true : false;
+    }
+    if (number_of_cells_to_move_ == 0) {
+      if (qrand() % 2 == 0) {
+        TurnRotationReverseOn();
+      } else {
+        TurnRotationReverseOff();
+      }
+      number_of_turns_ = amount_of_turns_;
+      return true;
+    }
+  }
+  return false;
+}
+
+bool Bot::IsMoveNeeded() const {
+  return time_to_finish_movement_ > 0;
+}
+
+bool Bot::IsMovingStartNeeded() {
+  if (time_to_finish_movement_ <= 0 && time_to_finish_rotation_ <= 0) {
+    if (number_of_cells_to_move_ == 0) {
+      if (number_of_turns_ == 0) {
+        number_of_cells_to_move_ = moving_length_;
+      } else {
+        return false;
+      }
+    } else {
+      number_of_cells_to_move_--;
+    }
+    return number_of_cells_to_move_ > 0 ? true : false;
+  }
+  return false;
 }
 
 bool Bot::IsShotNeeded(std::shared_ptr<Map> map,
-                       std::shared_ptr<Tank> tank) const {
-  if (time_to_finish_rotation_ <= 0) {
+                       std::shared_ptr<Tank> tank) {
+  if (time_to_finish_rotation_ == 0 && time_to_finish_movement_ == 0) {
     int direction = GetIntDirection();
+    int tank_x = tank->GetCellX();
+    int tank_y = tank->GetCellY();
+    int bot_x = GetCellX();
+    int bot_y = GetCellY();
+
     if (direction == 0 || direction == 2) {
-      if (tank->GetCellX() == GetCellX()) {
-        int delta = (direction == 0) ? -1 : 1;
-        int finish_cell =
-            (direction == 0) ? 0 : map->GetNumberOfCellsVertically();
-        int curr_cell = GetCellY() + delta;
-        while (curr_cell != finish_cell &&
-               map_->GetField(GetCellX(), curr_cell) != CellType::Wall) {
-          if (tank->GetCellY() == curr_cell) {
-            return true;
-          }
-          curr_cell += delta;
+      if (tank_x == bot_x) {
+        int walls_count = map->GetWallsPrecalc(bot_x, bot_y);
+        walls_count += map->GetWallsPrecalc(tank_x - 1, tank_y - 1);
+        walls_count -= map->GetWallsPrecalc(tank_x, tank_y - 1);
+        walls_count -= map->GetWallsPrecalc(bot_x - 1, bot_y);
+        if (walls_count != 0) {
+          return false;
+        }
+
+        if (CheckDirection(tank_y, bot_y, direction)) {
+          return true;
         }
       }
     }
     if (direction == 1 || direction == 3) {
-      if (tank->GetCellY() == GetCellY()) {
-        int delta = (direction == 3) ? -1 : 1;
-        int finish_cell =
-            (direction == 3) ? 0 : map->GetNumberOfCellsHorizontally();
-        int curr_cell = GetCellX() + delta;
-        while (curr_cell != finish_cell &&
-               map_->GetField(curr_cell, GetCellY()) != CellType::Wall) {
-          if (tank->GetCellX() == curr_cell) {
-            return true;
-          }
-          curr_cell += delta;
+      if (tank_y == bot_y) {
+        int walls_count = map->GetWallsPrecalc(bot_x, bot_y);
+        walls_count += map->GetWallsPrecalc(tank_x - 1, tank_y - 1);
+        walls_count -= map->GetWallsPrecalc(tank_x - 1, tank_y);
+        walls_count -= map->GetWallsPrecalc(bot_x, bot_y - 1);
+        if (walls_count != 0) {
+          return false;
+        }
+
+        if (CheckDirection(tank_x, bot_x, direction)) {
+          return true;
         }
       }
     }
   }
   return false;
+}
+
+bool Bot::CheckDirection(int& tank, int& bot, int direction) {
+  if (tank > bot) {
+    std::swap(tank, bot);
+    if (direction == 0 || direction == 3) {
+      return false;
+    }
+  } else if (direction == 2 || direction == 1) {
+    return false;
+  }
+  return true;
 }
---
+++ improvedbot.cpp
@@ -0,0 +1,44 @@
+#include "improvedbot.h"
+
+ImprovedBot::ImprovedBot(std::shared_ptr<Map>& map, int init_cell_x, int init_cell_y, int speed,
+         int rate_of_fire, Direction direction, int moving_length, int amount_of_turns)
+    : Bot(map, init_cell_x, init_cell_y, speed, rate_of_fire, direction,
+          moving_length, amount_of_turns) {};
+
+bool ImprovedBot::IsRotationStartNeeded(std::shared_ptr<Tank> tank) {
+  if (time_to_finish_rotation_ <= 0 && time_to_finish_movement_ <= 0) {
+    if (number_of_turns_ > 0) {
+      number_of_turns_--;
+      return number_of_turns_ > 0 ? true : false;
+    }
+    if (number_of_cells_to_move_ == 0) {
+      if (IsShotNeeded(map_, tank)) {
+        return false;
+      }
+      if (qrand() % 2 == 0) {
+        TurnRotationReverseOn();
+      } else {
+        TurnRotationReverseOff();
+      }
+      number_of_turns_ = amount_of_turns_;
+      return true;
+    }
+  }
+  return false;
+}
+
+bool ImprovedBot::CheckDirection(int& tank, int& bot, int direction) {
+  if (tank > bot) {
+    std::swap(tank, bot);
+    if (direction == 0 || direction == 3) {
+      number_of_cells_to_move_ = 0;
+      number_of_turns_ = 3;
+      return false;
+    }
+  } else if (direction == 2 || direction == 1) {
+    number_of_cells_to_move_ = 0;
+    number_of_turns_ = 3;
+    return false;
+  }
+  return true;
+}
