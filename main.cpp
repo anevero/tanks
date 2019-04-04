037fa450f1d9fdfb13d728ee9e223c55984d9010
Andrew Nevero <andrew.nevero@outlook.com>
2019-04-04 18:43:39 +0300

    Set Fusion app window style
    Теперь на всех ОС стиль окна приложения будет таким же, как на Ubuntu.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 ﻿#include <QApplication>
+#include <QStyleFactory>
 #include "mainwindow.h"
 
 int main(int argc, char *argv[]) {
   QApplication a(argc, argv);
+  a.setStyle(QStyleFactory::create("Fusion"));
   MainWindow w;
   w.setWindowTitle("Tanks Alpha");
   w.show();
