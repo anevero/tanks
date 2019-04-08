24a99cb9191ce6af060ad87ea7f0a77da7577da7
Andrew Nevero <47552815+anevero@users.noreply.github.com>
2019-04-08 20:36:43 +0300

    Merge pull request #55
    * Кнопки и выпадающие меню настройки игры перенесены в отдельный диалог, чтобы не засорять пространство слева от карты.
    * Добавлен отдельный диалог настроек. Пока в нем можно лишь активировать или отключить виртуальные клавиши.
    * Добавлен автоматический перевод приложения на язык системы (если приложению удается распознать этот язык).

---
+++ translations/tanks_be_BY.ts
@@ -0,0 +1,124 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="be_BY">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.cpp" line="5"/>
+        <source>New game</source>
+        <translation>Новая гульня</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="6"/>
+        <location filename="../mainwindow.cpp" line="270"/>
+        <location filename="../mainwindow.cpp" line="332"/>
+        <source>Pause</source>
+        <translation>Паўза</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="7"/>
+        <source>Settings</source>
+        <translation>Налады</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="52"/>
+        <source>Health</source>
+        <translation>Здароўе</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="287"/>
+        <source>This level of difficulty isn&apos;t available on this map. 
+Try to switch to another map.</source>
+        <translation>Гэты ўзровень цяжкасці недаступны на абранай карце. Паспрабуйце пераключыцца на іншую карту.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="336"/>
+        <source>Continue</source>
+        <translation>Працягнуць</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="439"/>
+        <source>You win! 
+You can start a new game with help of appropriate button on the left.</source>
+        <translation>Вы перамаглі! Вы можаце пачаць новую гульню з дапамогаю адпаведнай кнопкі злева.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="444"/>
+        <source>You died! 
+You can start a new game with help of appropriate button on the left.</source>
+        <translation>Вы прайгралі! Вы можаце пачаць новую гульню з дапамогаю адпаведнай кнопкі злева.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="455"/>
+        <source>Choose map, tank and difficulty</source>
+        <translation>Выберыце карту, танк і цяжкасць</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="458"/>
+        <location filename="../mainwindow.cpp" line="464"/>
+        <source>Map</source>
+        <translation>Карта</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="470"/>
+        <location filename="../mainwindow.cpp" line="482"/>
+        <source>Tank</source>
+        <translation>Танк</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="487"/>
+        <source>Difficulty</source>
+        <translation>Цяжкасць</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="525"/>
+        <source>Activate virtual keys</source>
+        <translation>Актываваць віртуальныя клавішы</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="529"/>
+        <source>Language</source>
+        <translation>Мова</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="532"/>
+        <source>Default</source>
+        <translation>Па змаўчанні</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="533"/>
+        <source>Belarusian</source>
+        <translation>Беларуская</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="534"/>
+        <source>English</source>
+        <translation>English</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="535"/>
+        <source>Russian</source>
+        <translation>Русский</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="539"/>
+        <source>App version</source>
+        <translation>Версія</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.h" line="117"/>
+        <source>Easy</source>
+        <translation>Проста</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.h" line="117"/>
+        <source>Normal</source>
+        <translation>Нармальна</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.h" line="118"/>
+        <source>Hard</source>
+        <translation>Складана</translation>
+    </message>
+</context>
+</TS>
