889add78154984ddedc56289bd3e450cf9ad7f07
Andrew Nevero <andrew.nevero@outlook.com>
2019-04-08 21:33:49 +0300

    Fix bug with virtual keys

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -26,7 +26,7 @@ MainWindow::MainWindow(QWidget *parent)
 
   for (int i = 0; i < virtual_keys_buttons_.size(); ++i) {
     connect(virtual_keys_buttons_[i], &QPushButton::clicked,
-            [&]() { PressVirtualKey(virtual_keys_encodings_[i]); });
+            [this, i]() { PressVirtualKey(virtual_keys_encodings_[i]); });
     virtual_keys_buttons_[i]->setFocusPolicy(Qt::NoFocus);
   }
 
