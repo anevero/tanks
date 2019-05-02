53619a1308230cbaee7e005085ce2be0e2a0b62d
Andrew Nevero <andrew.nevero@outlook.com>
2019-05-02 13:30:21 +0300

    Update translations

--- translations/tanks_be_BY.ts
+++ translations/tanks_be_BY.ts
@@ -4,135 +4,163 @@
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="../mainwindow.cpp" line="6"/>
+        <location filename="../mainwindow.cpp" line="7"/>
         <source>New game</source>
         <translation>Новая гульня</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="7"/>
-        <location filename="../mainwindow.cpp" line="407"/>
-        <location filename="../mainwindow.cpp" line="505"/>
+        <location filename="../mainwindow.cpp" line="8"/>
+        <location filename="../mainwindow.cpp" line="470"/>
+        <location filename="../mainwindow.cpp" line="569"/>
         <source>Pause</source>
         <translation>Паўза</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="8"/>
+        <location filename="../mainwindow.cpp" line="9"/>
         <source>Settings</source>
         <translation>Налады</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="9"/>
+        <location filename="../mainwindow.cpp" line="10"/>
         <source>About</source>
         <translation>Інфармацыя</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="64"/>
+        <location filename="../mainwindow.cpp" line="40"/>
+        <source>You have</source>
+        <translation>У вас ёсць</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="42"/>
+        <source>minutes per round</source>
+        <translation>хвілін на гульню</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="82"/>
         <source>Hight speed, low charge, can destroy obstacles</source>
         <translation>Высокая хуткасць, нізкая шкода, можа знішчаць перашкоды</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="66"/>
+        <location filename="../mainwindow.cpp" line="84"/>
         <source>Medium speed, medium charge, can destroy obstacles</source>
         <translation>Сярэдняя хуткасць, сярэдняя шкода, можа знішчаць перашкоды</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="68"/>
+        <location filename="../mainwindow.cpp" line="86"/>
         <source>Low speed, hight charge, can&apos;t destroy obstacles</source>
         <translation>Нізкая хуткасць, высокая шкода, не можа знішчаць перашкоды</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="109"/>
+        <location filename="../mainwindow.cpp" line="129"/>
         <source>Health</source>
         <translation>Здароўе</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="509"/>
+        <location filename="../mainwindow.cpp" line="573"/>
         <source>Continue</source>
         <translation>Працягнуць</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="653"/>
+        <location filename="../mainwindow.cpp" line="728"/>
         <source>You win! 
 You can start a new game with help of appropriate button on the left.</source>
         <translation>Вы перамаглі! Вы можаце пачаць новую гульню з дапамогаю адпаведнай кнопкі злева.</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="658"/>
+        <location filename="../mainwindow.cpp" line="733"/>
         <source>You died! 
 You can start a new game with help of appropriate button on the left.</source>
         <translation>Вы прайгралі! Вы можаце пачаць новую гульню з дапамогаю адпаведнай кнопкі злева.</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="669"/>
+        <location filename="../mainwindow.cpp" line="744"/>
         <source>Choose map, tank and difficulty</source>
         <translation>Выберыце карту, танк і цяжкасць</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="672"/>
-        <location filename="../mainwindow.cpp" line="678"/>
+        <location filename="../mainwindow.cpp" line="747"/>
+        <location filename="../mainwindow.cpp" line="753"/>
         <source>Map</source>
         <translation>Карта</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="684"/>
-        <location filename="../mainwindow.cpp" line="701"/>
+        <location filename="../mainwindow.cpp" line="759"/>
+        <location filename="../mainwindow.cpp" line="776"/>
         <source>Tank</source>
         <translation>Танк</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="705"/>
+        <location filename="../mainwindow.cpp" line="780"/>
         <source>Difficulty</source>
         <translation>Цяжкасць</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="745"/>
+        <location filename="../mainwindow.cpp" line="820"/>
         <source>Activate virtual keys</source>
         <translation>Віртуальныя клавішы</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="749"/>
+        <location filename="../mainwindow.cpp" line="824"/>
+        <source>Activate charge line</source>
+        <translation>Паласа перазарадкі</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="827"/>
+        <source>Performance</source>
+        <translation>Прадукцыйнасць</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="832"/>
+        <source>frames per second</source>
+        <translation>кадраў у секунду</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="836"/>
         <source>Language</source>
         <translation>Мова</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="758"/>
+        <location filename="../mainwindow.cpp" line="844"/>
+        <source>Language will be changed after application restart</source>
+        <translation>Мова зменіцца пасля перазапуску прыкладання</translation>
+    </message>
+    <message>
         <source>Language will be changed
 after application restart</source>
-        <translation>Мова зменіцца пасля
+        <translation type="vanished">Мова зменіцца пасля
 перазапуску прыкладання</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="752"/>
+        <location filename="../mainwindow.cpp" line="839"/>
         <source>Belarusian</source>
         <translation>Беларуская</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="753"/>
+        <location filename="../mainwindow.cpp" line="840"/>
         <source>English</source>
         <translation>English</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="754"/>
+        <location filename="../mainwindow.cpp" line="841"/>
         <source>Russian</source>
         <translation>Русский</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="763"/>
         <source>App version</source>
-        <translation>Версія</translation>
+        <translation type="vanished">Версія</translation>
     </message>
     <message>
-        <location filename="../mainwindow.h" line="156"/>
+        <location filename="../mainwindow.h" line="177"/>
         <source>Easy</source>
         <translation>Проста</translation>
     </message>
     <message>
-        <location filename="../mainwindow.h" line="156"/>
+        <location filename="../mainwindow.h" line="177"/>
         <source>Normal</source>
         <translation>Нармальна</translation>
     </message>
     <message>
-        <location filename="../mainwindow.h" line="157"/>
+        <location filename="../mainwindow.h" line="178"/>
         <source>Hard</source>
         <translation>Складана</translation>
     </message>
